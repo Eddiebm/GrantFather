e0b48d86b762b55ee89a3ddd005dcbe782d3f3c6
Eddiebm <eddie@bannermanmenson.com>
2026-01-31 11:05:45 -0600

    Add Home component with title and description

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 50, textAlign: 'center' }}>
+      <h1 style={{ fontSize: 48 }}>GrantFather</h1>
+      <p style={{ fontSize: 20, color: '#666' }}>AI Grant Platform</p>
+    </div>
+  );
+}
